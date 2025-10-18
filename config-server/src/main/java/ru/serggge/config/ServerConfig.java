62b858db711ba2a09b4cf93de3af139a9aeb8aa6
Serggge <serggge69@yandex.ru>
2025-10-18 22:14:16 +0300

    reserve save

--- config-server/src/main/java/ru/serggge/config/ServerConfig.java
+++ config-server/src/main/java/ru/serggge/config/ServerConfig.java
@@ -3,7 +3,7 @@ package ru.serggge.config;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods = false)
 @EnableConfigServer
 public class ServerConfig {
 }
--- user-service/src/main/java/ru/serggge/config/AspectConfig.java
+++ user-service/src/main/java/ru/serggge/config/AspectConfig.java
@@ -1,8 +1,12 @@
 package ru.serggge.config;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import ru.serggge.aspect.MicrometerAspect;
 import ru.serggge.aspect.NotificationAspect;
 
 @Configuration
@@ -12,5 +16,15 @@ public class AspectConfig {
     @Bean
     public NotificationAspect notificationAspect() {
         return new NotificationAspect();
+    }
+
+    @Bean
+    public MicrometerAspect micrometerAspect(MeterRegistry meterRegistry) {
+        return new MicrometerAspect(meterRegistry);
+    }
+
+    @Bean
+    public ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
+        return new ObservedAspect(observationRegistry);
     }
 }
