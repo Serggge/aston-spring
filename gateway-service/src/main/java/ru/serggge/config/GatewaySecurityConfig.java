9f4a7eabb9bcf38a360219c5cdd70ca0ca8592fe
Serggge <serggge69@yandex.ru>
2025-10-22 13:43:48 +0300

    oauth server done

---
+++ gateway-service/src/main/java/ru/serggge/config/GatewaySecurityConfig.java
@@ -0,0 +1,31 @@
+package ru.serggge.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain defaultSecurityFilterChain(ServerHttpSecurity http) throws Exception {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange()
+                        .authenticated())
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
